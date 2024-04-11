8292e18eed1625d5e357f64d6c76c926bbc10bd9
Xavier Massaneda Armengol <xaviermassa111@gmail.com>
2024-04-11 11:35:36 +0200

    Movimiento

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float runSpeed = 2;
+    public float jumpSpeed = 3;
+    Rigidbody2D rb;
+
+    public bool isGrounded = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey("d"))
+        {
+            rb.velocity = new Vector2(runSpeed, rb.velocity.y);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.velocity = new Vector2(-runSpeed, rb.velocity.y);
+        }
+        if (Input.GetKey("w") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Flor"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
